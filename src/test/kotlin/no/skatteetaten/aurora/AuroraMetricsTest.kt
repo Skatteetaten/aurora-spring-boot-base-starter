2077a033377c29d811caa0b37e37f603bf255cbc
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-03-27 15:18:48 +0100

    added test for thrown exception

--- src/test/kotlin/no/skatteetaten/aurora/AuroraMetricsTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/AuroraMetricsTest.kt
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import java.lang.RuntimeException
 
 @TestConfiguration
 open class TestConfig {
@@ -34,5 +35,16 @@ class AuroraMetricsTest {
 
         assertThat(registry.get("operations").timer()).isNotNull()
         assertThat(registry.get("statuses").counters()).hasSize(2)
+    }
+
+    @Test
+    fun `Register timer when exception is thrown`() {
+        kotlin.runCatching {
+            metrics.withMetrics("unit-test") {
+                throw RuntimeException("test exception")
+            }
+        }
+
+        assertThat(registry.get("operations").timer()).isNotNull()
     }
 }
