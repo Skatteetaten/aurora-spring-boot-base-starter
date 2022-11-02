96ae7c1bd53da85820a68fab7d8522b1f4666012
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-11-02 13:10:35 +0100

    Use latest aurora gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import no.skatteetaten.aurora.gradle.plugins.extensions.implementationTransitiveNexusIQIssue
+
 plugins {
     `java-library`
     kotlin("jvm") version "1.7.10"
-    id("no.skatteetaten.gradle.aurora") version "4.5.6"
+    id("no.skatteetaten.gradle.aurora") version "4.5.8"
 }
 
 aurora {
@@ -15,8 +17,10 @@ aurora {
 }
 
 dependencies {
+    implementationTransitiveNexusIQIssue("org.yaml:snakeyaml:1.33")
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    
+
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
