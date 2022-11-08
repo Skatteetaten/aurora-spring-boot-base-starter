d935b50a6daaa1592039dd5a0e67f01284777b82
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-11-08 08:48:31 +0100

    removed unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     `java-library`
     kotlin("jvm") version "1.7.20"
     id("no.skatteetaten.gradle.aurora") version "4.5.10"
-    id("io.spring.dependency-management") version "1.1.0"
 }
 
 aurora {
@@ -20,14 +19,7 @@ aurora {
 }
 
 dependencies {
-    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.5"))
-
-    implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
-    implementation("org.springframework.cloud:spring-cloud-sleuth-brave")
-    implementation("org.springframework.cloud:spring-cloud-sleuth-zipkin")
-
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-registry-prometheus")
 
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
